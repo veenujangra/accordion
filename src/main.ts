6bf12fc44aa6e2164c809d35e73b9f00132baad3
veenujangra <veenuj01@gmail.com>
2025-05-14 08:00:04 +0530

    first commit

---
+++ src/main.ts
@@ -0,0 +1,119 @@
+import gsap from 'gsap'
+import { each, map } from 'lodash'
+
+interface AccordionOptions {
+  elements: string
+}
+
+interface AccordionItem {
+  header: HTMLElement
+  body: HTMLElement
+  status: boolean
+}
+
+export default class Accordion {
+  private elements: NodeListOf<HTMLElement>
+  private accordionItems: AccordionItem[] = []
+  private tl: gsap.core.Timeline | null = null
+
+  constructor(options: AccordionOptions) {
+    this.elements = document.querySelectorAll(options.elements)
+    console.log(this.elements)
+    this.create()
+    this.setProperties()
+  }
+
+  create() {
+    this.elements.forEach((element: HTMLElement) => {
+      const items = element.querySelectorAll('[data-accordion="item"]')
+
+      console.log(items)
+
+      this.accordionItems = map(items, (item: Element): AccordionItem => {
+        const header = item.querySelector('[data-accordion="header"]') as HTMLElement
+        const body = item.querySelector('[data-accordion="body"]') as HTMLElement
+
+        return {
+          header,
+          body,
+          status: true,
+        }
+      })
+
+      each(this.accordionItems, (_item: AccordionItem, index: number) => {
+        this.handleClick(index)
+      })
+    })
+  }
+
+  setProperties() {
+    this.resetAccordion()
+    console.log(this.accordionItems)
+    this.accordionOpen(this.accordionItems[0].body, 0)
+  }
+
+  handleClick(index: number) {
+    const header = this.accordionItems[index].header
+    const body = this.accordionItems[index].body
+
+    header.addEventListener('click', () => {
+      const status = this.accordionItems[index].status
+      if (status === true) {
+        this.accordionClose(body, index)
+      } else {
+        this.accordionOpen(body, index)
+      }
+    })
+  }
+
+  accordionOpen(body: HTMLElement, index: number) {
+    this.tl = gsap.timeline({
+      onStart: () => {
+        this.resetAccordion()
+        this.accordionItems[index].status = true
+        this.accordionItems[index].header.setAttribute('aria-expanded', 'true')
+      },
+    })
+
+    this.tl.fromTo(
+      body,
+      {
+        height: 0,
+      },
+      {
+        height: 'auto',
+        duration: 0.43,
+        ease: 'power3.inOut',
+      }
+    )
+  }
+
+  accordionClose(body: HTMLElement, index: number) {
+    this.tl = gsap.timeline({
+      onStart: () => {
+        this.accordionItems[index].status = false
+        this.accordionItems[index].header.setAttribute('aria-expanded', 'false')
+      },
+    })
+
+    this.tl.to(body, {
+      height: 0,
+      duration: 0.4,
+      ease: 'power3.inOut',
+    })
+  }
+
+  resetAccordion() {
+    each(this.accordionItems, (item: AccordionItem) => {
+      if (item.status === true) {
+        this.accordionClose(item.body, this.accordionItems.indexOf(item))
+      }
+    })
+  }
+
+  addEventListeners() {}
+}
+
+new Accordion({
+  elements: '[data-accordion=list]' as string,
+})
